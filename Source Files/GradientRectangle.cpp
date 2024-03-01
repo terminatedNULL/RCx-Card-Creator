99822232e4930e6b9f5249eee707c6f70ab18eb5
AlexanderYauchler <135302972+terminatedNULL@users.noreply.github.com>
2024-03-01 01:02:42 -0500

    Member variable renaming
    Almost renamed every instance of m_points in imgui and SFML headers (°ー°〃)

--- Source Files/GradientRectangle.cpp
+++ Source Files/GradientRectangle.cpp
@@ -1,19 +1,19 @@
 #include "GradientRectangle.hpp"
 
 GradientRectangle::GradientRectangle(int x, int y, int width, int height) {
-	points[0] = sf::Vertex(sf::Vector2f(x, y), sf::Color::Black);
-	points[1] = sf::Vertex(sf::Vector2f(x + width, y), sf::Color::Black);
-	points[2] = sf::Vertex(sf::Vector2f(x + width, y + height), sf::Color::Black);
-	points[3] = sf::Vertex(sf::Vector2f(x, y + height), sf::Color::Black);
+	m_points[0] = sf::Vertex(sf::Vector2f(x, y), sf::Color::Black);
+	m_points[1] = sf::Vertex(sf::Vector2f(x + width, y), sf::Color::Black);
+	m_points[2] = sf::Vertex(sf::Vector2f(x + width, y + height), sf::Color::Black);
+	m_points[3] = sf::Vertex(sf::Vector2f(x, y + height), sf::Color::Black);
 }
 
 void GradientRectangle::SetColor(sf::Color c1, sf::Color c2, sf::Color c3, sf::Color c4) {
-	points[0].color = c1;
-	points[1].color = c2;
-	points[2].color = c3;
-	points[3].color = c4;
+	m_points[0].color = c1;
+	m_points[1].color = c2;
+	m_points[2].color = c3;
+	m_points[3].color = c4;
 }
 
 void GradientRectangle::Draw(sf::RenderWindow& wind) {
-	wind.draw(points, 4, sf::Quads);
+	wind.draw(m_points, 4, sf::Quads);
 }
